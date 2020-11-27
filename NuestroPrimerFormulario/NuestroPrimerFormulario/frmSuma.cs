9d2793307b4f35d43ff502bd6ad0c367eeb64b3b
Manuel Ferrus <manuel.ferrus@gmail.com>
2020-11-26 19:23:06 -0700

    Ejercicio de Suma
    Se sube el ejercicio de suma

---
+++ NuestroPrimerFormulario/NuestroPrimerFormulario/frmSuma.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NuestroPrimerFormulario
+{
+    public partial class frmSuma : Form
+    {
+        public frmSuma( )
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click( object sender, EventArgs e )
+        {
+            decimal numero1= txtNumero1.Value;
+            decimal numero2= txtNumero2.Value;
+            decimal numero3= txtNumero3.Value;
+            decimal suma=0;
+
+            if ( numero1 == 0 || numero2 == 0 || numero3 == 0 )
+            {
+                MessageBox.Show( "El número no puede ser cero", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Exclamation );
+                return; //se sale del flujo
+            }
+            else
+            {
+                suma = numero1 + numero2 + numero3;
+            }
+
+            lblRespuesta.Text = "La suma es: " + suma;
+        }
+
+        private void btnLimpiar_Click( object sender, EventArgs e )
+        {
+            lblRespuesta.Text = "";
+            txtNumero1.Value = 0;
+            txtNumero2.Value = 0;
+            txtNumero3.Value = 0;
+        }
+    }
+}
