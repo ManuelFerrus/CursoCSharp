2cb5ea1585ab0c6a9c6ee0959bd40360f4c9106c
Manuel Ferrus <manuel.ferrus@gmail.com>
2020-12-22 17:42:42 -0700

    Introducion LinQ
    Primer ejercicio con consulta LinQ
    * NOTA: tiene que ser con visual 2015 o superior

---
+++ CursoLinQ2019/CursoLinQ2019/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoLinQ2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 fuent de datos
+            int[] numeros = { 99, 56, 75, 45, 12 };
+            //2 definimos la consulta
+            var consulta = from numero in numeros
+                           where numero > 70
+                           select numero;//consulta LinQ
+            //3 Ejecucion de consulta
+            foreach (var item in consulta)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
